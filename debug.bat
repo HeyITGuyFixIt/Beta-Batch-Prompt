9526e6f0859e9b5bbac2813d8ef792f95d24e903
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2018-10-23 09:09:04 -0400

    Update debug.bat

--- debug.bat
+++ debug.bat
@@ -1,8 +1,8 @@
 :TOP
 @ECHO OFF
 @TITLE Debug Mode
-@ECHO Debug Mode...
+@ECHO:Debug Mode...
 @CLS && call "Batch Prompt.bat"
 @PAUSE
-SET /P "EXIT=Quit Debug Session? (y/n) "
-IF /I "%EXIT%"=="y" (EXIT) ELSE (GOTO TOP)
+@SET /P "EXIT=Quit Debug Session? (y/n) "
+@IF /I "%EXIT%"=="y" (EXIT) ELSE (GOTO TOP)
